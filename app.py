72b229fb652776b27e27b7d1f324d426571a0299
Sathwik <sathwik2104@gmail.com>
2024-12-08 11:13:24 +0530

    modified

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ counter = 0
 
 def main():
     global counter
-    st.title("Intents of Chatbot using NLP")
+    st.title("Movie Recommendation bot")
 
     # Create a sidebar menu with options
     menu = ["Home", "Conversation History", "About"]
